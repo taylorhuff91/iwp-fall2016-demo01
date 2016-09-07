3b91b05575b9c86c9c643373873a8c0b9f989561
Taylor Huff <dancing_floie@yahoo.com>
2016-09-07 15:13:52 +0000

    Finished Demo 01, TH

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 // Use the scenario below to output your Mad Lib.
 /* Dear [name], I’m writing to inform you that [firstName] [surName] will not be attending the annual [noun] party. [firstName] has come down with a bad case of [illness] and is running a [number] degree fever. We have an appointment with Dr. [emotion] tomorrow at [theTime]. Thanks for your [verb]ing! */
+document.write("Hello World!");
